70c9c22b61f6949bd32b00d671b6af98f579db9f
franvico <franvico.dev@gmail.com>
2024-11-12 23:07:19 +0100

    Practica XML terminada

--- Desarrollo de Interfaces/Repositorio/Practica2_XML/Practica2_XML/Program.cs
+++ Desarrollo de Interfaces/Repositorio/Practica2_XML/Practica2_XML/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Practica2_XML.Modelos;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Policy;
 using System.Text;
@@ -14,6 +15,8 @@ namespace Practica2_XML
 
         static string xmlPath = "universidad.xml";
         static string xmlPathMod = "universidadMod.xml";
+        static string xsdPath = "universidad.xsd";
+
         static void Main(string[] args)
         {
             bool exit = false;
@@ -25,6 +28,7 @@ namespace Practica2_XML
                 Console.WriteLine("1. Consultar todas las asignaturas");
                 Console.WriteLine("2. Añadir nueva asignatura");
                 Console.WriteLine("3. Buscar alumnos por asignatura específica");
+                Console.WriteLine("4. Validar archivo xml con xsd");
                 Console.WriteLine("0. Salir\n");
 
                 // Leer la opción seleccionada
@@ -41,7 +45,10 @@ namespace Practica2_XML
                             crearAsignatura();
                             break;
                         case 3:
-                            //GetAsignaturaMasCara();
+                            consultarAlumnosPorAsignatura();
+                            break;
+                        case 4:
+                            validarXML();
                             break;
                         case 0:
                             Console.WriteLine("Adiós");
@@ -58,6 +65,82 @@ namespace Practica2_XML
                 }
             }
 
+        }
+
+        public static void validarXML()
+        {
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.Schemas.Add(null, xsdPath);
+            settings.ValidationType = ValidationType.Schema;
+
+            XmlReader reader = XmlReader.Create(xmlPath, settings);
+
+            try
+            {
+                while (reader.Read())
+                {
+
+                }
+                Console.WriteLine("El archivo XML es válido.");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error de XML: " + e.Message);
+            }
+        }
+
+        public static void consultarAlumnosPorAsignatura()
+        {
+            Dictionary<string, XmlNode> asignaturas = consultarAsignaturasExistentes();
+
+            Console.WriteLine("\nESCRIBE EL NOMBRE DE LA ASIGNATURA A CONSULTAR:\n");
+            foreach(string nombreAsignatura in asignaturas.Keys)
+            {
+                Console.WriteLine("- " + nombreAsignatura);
+            }
+
+            mostrarAlumnos(Console.ReadLine(), asignaturas);
+
+        }
+
+        public static void mostrarAlumnos(string nombreAsignatura, Dictionary<string, XmlNode> asignaturas)
+        {
+            if (!asignaturas.Keys.Contains(nombreAsignatura))
+            {
+                Console.WriteLine("\nLa asignatura solicitada no existe");
+                consultarAlumnosPorAsignatura();
+            }
+            else
+            {
+                Console.WriteLine("\nAlumnos matriculados en " + asignaturas[nombreAsignatura].Attributes["nombre"].Value + "\n");
+
+                XmlNodeList nodosAlumno = asignaturas[nombreAsignatura].SelectNodes("alumno");
+                foreach(XmlNode nodoAlumno in nodosAlumno)
+                {
+                    Console.WriteLine("\tNombre: " + nodoAlumno["nombre"].InnerText);
+                    Console.WriteLine("\tEdad: " + nodoAlumno["edad"].InnerText);
+                    Console.WriteLine("\tGrado: " + nodoAlumno["grado"].InnerText + "\n");
+                }
+            }
+        }
+
+        public static Dictionary<string, XmlNode> consultarAsignaturasExistentes()
+        {
+            Dictionary<string, XmlNode> nombresAsignaturas = new Dictionary<string, XmlNode>();
+
+            // cargo el fichero
+            XmlDocument xmlDoc = cargarFicheroXML();
+
+            // saco una lista de nodos asignatura
+            XmlNodeList nodosAsignatura = xmlDoc.SelectNodes("universidad/asignatura");
+
+            // recorro todos y guardo su atributo "nombre"
+            foreach(XmlNode nodoAsignatura in nodosAsignatura)
+            {
+                nombresAsignaturas.Add(nodoAsignatura.Attributes["nombre"]?.Value, nodoAsignatura);
+            }
+
+            return nombresAsignaturas;
         }
 
         public static void crearAsignatura()
@@ -86,8 +169,7 @@ namespace Practica2_XML
                             listaAlumnos.Add(crearAlumno());
                             break;
                         case 2:
-                            // añade los nuevos datos al fichero
-                            asignatura.setAlumnos(listaAlumnos);
+                            asignatura.setAlumnos(listaAlumnos);                            
                             modificarFicheroXML(asignatura);
                             exit = true;
                             break;
@@ -106,12 +188,44 @@ namespace Practica2_XML
 
         public static void modificarFicheroXML(Asignatura asignatura)
         {
-            
-            XmlDocument xmlDoc = cargarFicheroXML(xmlPath);
+            XmlDocument xmlDoc = cargarFicheroXML();
 
-            // creo un nuevo elemento a partir del objeto asignatura
-            XmlElement asignaturaElement = xmlDoc.CreateElement("asignatura");
-            asignaturaElement.SetAttribute("nombre", asignatura.nombre);
+            // creo un nuevo elemento <asignatura>
+            XmlElement elementoAsignatura = xmlDoc.CreateElement("asignatura");
+            elementoAsignatura.SetAttribute("nombre", asignatura.nombre); // añado su atributo con los datos del objeto Asignatura
+
+            foreach(Alumno alumno in asignatura.getAlumnos())
+            {
+                // creo un nuevo elemento <alumno>
+                XmlElement elementoAlumno = xmlDoc.CreateElement("alumno");
+
+                // creo los elementos <nombre> <edad> y <grado>
+                XmlElement elementoNombre = xmlDoc.CreateElement("nombre");
+                XmlElement elementoEdad = xmlDoc.CreateElement("edad");
+                XmlElement elementoGrado = xmlDoc.CreateElement("grado");
+
+                // asigno el valor de cada uno a partir de los datos del objeto Alumno
+                elementoNombre.InnerText = alumno.nombre;
+                elementoEdad.InnerText = alumno.edad.ToString();
+                elementoGrado.InnerText = alumno.grado.ToString();
+
+                // añado estos elementos al elemento <alumno>
+                elementoAlumno.AppendChild(elementoNombre);
+                elementoAlumno.AppendChild(elementoEdad);
+                elementoAlumno.AppendChild(elementoGrado);
+
+                // añado el elemento <alumno> ya seteado al elemento <asignatura>
+                elementoAsignatura.AppendChild(elementoAlumno);
+            }
+
+            // selecciono el nodo raíz (universidad)
+            XmlNode nodoUniversidad = xmlDoc.SelectSingleNode("universidad");
+
+            // añado el elemento <asignatura> al nodo <universidad>
+            nodoUniversidad.AppendChild(elementoAsignatura);
+
+            // guardo el documento en una nueva ruta
+            xmlDoc.Save(xmlPathMod);
 
 
         }
@@ -131,7 +245,7 @@ namespace Practica2_XML
 
         public static void consultarAsignaturas()
         {
-            XmlDocument xmlDoc = cargarFicheroXML(xmlPath);
+            XmlDocument xmlDoc = cargarFicheroXML();
 
             // recojo todos los nodos "asignatura" dentro del nodo raíz
             XmlNodeList asignaturas = xmlDoc.SelectNodes("universidad/asignatura");
@@ -153,13 +267,15 @@ namespace Practica2_XML
             }            
         }
 
-        public static XmlDocument cargarFicheroXML(string filePath)
+        public static XmlDocument cargarFicheroXML()
         {
             // crear objeto XmlDocument
             XmlDocument xmlDoc = new XmlDocument();
 
-            // cargo el contenido de mi xml en el objeto XmlDoc
-            xmlDoc.Load(filePath);
+            // compruebo si ya tengo un fichero universidadMod.xml (no quiero sobreescribir el original de momento)
+            string path = File.Exists(xmlPathMod) ? xmlPathMod: xmlPath;
+            // cargo el fichero
+            xmlDoc.Load(path);
 
             return xmlDoc;
         }
