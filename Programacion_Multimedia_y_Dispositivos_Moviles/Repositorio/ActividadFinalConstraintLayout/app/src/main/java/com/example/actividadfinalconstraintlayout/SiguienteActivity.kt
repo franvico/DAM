563968b410b8f4778ca3ea26e3c9f478857450ab
franvico <franvico.dev@gmail.com>
2024-12-04 12:34:40 +0100

    actividad final constraint layout

---
+++ Programacion_Multimedia_y_Dispositivos_Moviles/Repositorio/ActividadFinalConstraintLayout/app/src/main/java/com/example/actividadfinalconstraintlayout/SiguienteActivity.kt
@@ -0,0 +1,9 @@
+package com.example.actividadfinalconstraintlayout
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SiguienteActivity : AppCompatActivity() {
+}
