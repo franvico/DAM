dc02076471a3c3489f365adafd8a90bd404f405a
franvico <franvico.dev@gmail.com>
2025-01-08 13:00:36 +0100

    cambios

--- Programacion_Multimedia_y_Dispositivos_Moviles/Repositorio/2.1 CalculadoraConstraintLayout/app/build.gradle.kts
+++ Programacion_Multimedia_y_Dispositivos_Moviles/Repositorio/2.1 CalculadoraConstraintLayout/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.calculadoraconstraintlayout"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.calculadoraconstraintlayout"
